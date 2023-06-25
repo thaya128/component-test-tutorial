fe2c3db10aed3a7c94cb6577f3ee3f2ec4b8a251
Tomoya Hayashi <tomoya128@gmail.com>
2023-06-25 17:23:35 +0900

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SimpleButton } from "./SimpleButton";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SimpleButton />
   </React.StrictMode>
 );
 
