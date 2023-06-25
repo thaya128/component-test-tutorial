a3c5ea13d542f8fb952031d3a3cbd0da9af57589
Tomoya Hayashi <tomoya128@gmail.com>
2023-06-25 19:09:08 +0900

    snapshot testに修正

--- src/SimpleButton.test.tsx
+++ src/SimpleButton.test.tsx
@@ -1,12 +1,7 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render } from "@testing-library/react";
 import { SimpleButton } from "./SimpleButton";
 
-test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
-  const user = userEvent.setup();
-  render(<SimpleButton />);
-  const simpleButton = screen.getByRole("button");
-  expect(simpleButton).toHaveTextContent("OFF");
-  await user.click(simpleButton);
-  expect(simpleButton).toHaveTextContent("ON");
+test("描画されてすぐはOFFと表示されている", () => {
+  const view = render(<SimpleButton />);
+  expect(view.container).toMatchSnapshot();
 });
